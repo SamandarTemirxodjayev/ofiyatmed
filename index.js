820b31370075588c8b03cb008270dcf0816d1d40
Samandar <iwtuz166@gmail.com>
2023-08-03 21:41:52 +0500

    ssl

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const router = require('./routes/router');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use('/', router)
+app.use('/', router);
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/fullchain.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/chain.pem')
+};
+
+const server = https.createServer(options, app);
+
+const port = 443; 
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
