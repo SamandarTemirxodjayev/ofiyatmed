eda805c6b01b33f3c613b730b75f0ed3cdc454b7
Samandar <iwtuz166@gmail.com>
2023-08-03 22:44:36 +0500

    add about page

--- index.js
+++ index.js
@@ -25,10 +25,3 @@ server.listen(port, () => {
 app.use((req, res, next) => {
   res.status(404).render('404');
 });
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  } else {
-    next();
-  }
-});
--- routes/router.js
+++ routes/router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.index);
+router.get('/about', userController.about);
 
 module.exports = router;
