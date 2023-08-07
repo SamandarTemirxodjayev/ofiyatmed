3a15a195eac985376344110067105f34b72c04f3
Samandar <iwtuz166@gmail.com>
2023-08-07 22:04:53 +0500

    fix some errors

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const router = require('./routes/router');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/', router);
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/fullchain.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/arkengenerator.uz/chain.pem')
-};
-
-const server = https.createServer(options, app);
-
-const port = 443; 
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb://127.0.0.1:27017/ofiyatmed?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Server is connecting on MongoDB");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
+    })
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
 app.use((req, res, next) => {
   res.status(404).render('404');
 });
